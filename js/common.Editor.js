59ff771199d26cbf1eeaed9987e7f6ff05b4537f
liuli0803 <2223204922@qq.com>
2017-03-24 14:47:15 +0800

    RD改右边去

--- js/common.Editor.js
+++ js/common.Editor.js
@@ -1,7 +1,7 @@
 /**
  * Created by 大丽丽 on 2017/3/22.
  */
-//1---引用文件
+
 //section--组件
 //$('.secPart').on('click',function() {
 //    var ref = $(this).attr('name');
@@ -18,6 +18,8 @@
 //    });
 //});
 
+
+//1---引用文件
 function testLeft(){
     $.ajax({
         async : false,
@@ -42,36 +44,37 @@ function testLeftCallBack(data){
             str= '<div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title">'
             str+='<a data-toggle="collapse" data-parent="#accordionPanels" href="#'+list[i].blockType+'">列表组件 </a></h4></div>'
             str+='<div id="'+list[i].blockType+'" class="panel-collapse collapse in">'
-            str+='<div class="panel-body"><span onclick="upload(\''+list[i].blockPath+'\')" class="secPart" id="'+list[i].blockId+'" name="list01">'+list[i].blockName+'</span></div></div></div>'
-            //str+='<div class="panel-body"><span onclick="importFile(\''+list[i].blockId+'\')" class="secPart" id="'+list[i].blockId+'" name="list01">'+list[i].blockName+'</span></div></div></div>'
+            str+='<div class="panel-body"><span onclick="importFile(\''+list[i].blockId+'\')" class="secPart" id="'+list[i].blockId+'" name="list01">'+list[i].blockName+'</span></div></div></div>'
 
         }
-
-        //importFile(list[i].blockId,list[i].blockPath);
-
         $('.partAll .panel-group').append(str);
     }
 
-
 }
 
-function importFile(url ){
+function importFile(id ){
 
+    $.get("editorSection/" + id + ".html", function (data) {
+        $('.editorSection div.row').append(data);
+        $('.newSection').dashboard();
+    });
 
-    var fso = new ActiveXObject("Scripting.FileSystemObject");
-    var f1 = fso.GetFile(url);
-    console.log(url);
-    console.log("File last modified: " + f1.DateLastModified);
-
-    //$.get("editorSection/" + id + ".html", function (data) {
-    //    $('.editorSection div.row').append(data);
-    //    $('.newSection').dashboard();
-    //})
-
+    //$.getJSON({
+    //    async : true,
+    //    cache:true,
+    //    type: 'get',
+    //    dataType : "html",
+    //    //data:{dataType:"jsonp",blockId:id},  //参数
+    //    url:"http://192.168.31.156:8090/TemplateEditor/editorSection/lhbb.html?callback=?",//请求的action路径
+    //    error: function () {//请求失败处理函数
+    //
+    //    },
+    //    success:function(data){ //请求成功后处理函数。
+    //        console.log(data)
+    //    }
+    //});
+    //http://192.168.31.156:8090/TemplateEditor/editorSection/lhbb.html
 }
-
-
-
 
 
 //2---组件显示状态
@@ -79,10 +82,9 @@ $('.indexAll').on("click",".appendStr .panel",function(){
     $(".indexAll .appendStr .panel").removeClass('appendCur');
     $(this).addClass("appendCur");
 
-    var id=$(this).attr("id");
-    //console.log(id);
+    var id=$(this).parent().attr("id");
+    console.log(id)
     testRight(id);
-//        alert(2)
 
 });
 
@@ -103,23 +105,55 @@ function testRight(id){
     });
 }
 function testRightCallBack(data){
-    //console.log(data);
+    console.log(data.dataList);
     var list=data.dataList;
+    $(".property").html("");
     for( var i=0;i<list.length;i++ ){
         var str="";
-        if( list[i].type=="text" ){
-            str='<div class="row control-group"><label class="control-label col-xs-3" for="listName">'+list[i].name+'</label>'
-            str+='<div class="controls col-xs-9"><input type="text" id="'+list[i].id+'" class="form-input" placeholder="区块名称"></div></div>'
+        if( list[i].propertyType=="text" ){
+            str='<div class="row control-group"><label class="control-label col-xs-3" for="'+list[i].propertyId+'">'+list[i].propertyName+'</label>'
+            str+='<div class="controls col-xs-9"><input onchange="collectProperty(this)" type="text" id="'+list[i].propertyId+'" class="form-input" placeholder="区块名称"></div></div>'
         }
-        //console.log(str)
+        console.log(str)
+        console.log( "html="+$(".property").html() )
+
         $('.property').append(str);
 
     }
 }
 
+var obj = {"header":[],"section":[],"footer":[]};
+
+function collectProperty( input ){
+//        var code=$(".appendCur#"+id).prop("outerHTML");
+
+    //组件id
+    var id=$('.appendCur #myId').val();
+    var showId=id+"_"+$(".editorSection #"+$('.appendCur #myId').val()).length;
+
+    //console.log( $(input).attr("id") );
+    //console.log( $(input).val() );
+    //console.log( "showId"+"="+showId );
+    //console.log( "id"+"="+id );
+
+    var option = {};
+    option.id = id;
+    option.showId = showId;
+    //option.type = type;
+    //option.listNum = listNum;
+    //option.listData = listData;
+
+
+    $(".appendCur").parent().attr("data-showId",option.showId);
+    console.log(  $(".appendCur").parent().attr("data-showId") )
+
+    obj.section.push(option);
+    //console.log(  (JSON.stringify(obj)) )
+}
+
 
 //提交button---属性
-var obj = {"header":[],"section":[],"footer":[]};
+
 $(".submitBtn").click(function(){
 
     var type=$(this).attr("data-group");
@@ -210,14 +244,6 @@ $(".submitBtn").click(function(){
 //    });
 //}
 function callback( data ){
-    //if( data.flag=="channel_list" ){
-    //    getChannelListCallback(data);
-    //}else if( data.flag=="channel_article_list_index" ){
-    //    textCallBack(data);
-    //}else if(data.flag=="testRight"){
-    //    testRightCallBack(data);
-    //}else
-
     var flag=data.flag;
     (eval(flag+'CallBack'))(data);
 
@@ -248,25 +274,6 @@ function callback( data ){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //存储container
 
 window.onload=function(){
@@ -275,38 +282,39 @@ window.onload=function(){
 
 
     $('#keep').click(function(){
-        alert(1);
+        //alert(1);
         var str=(JSON.stringify(obj)),
             order="";
         $(".editorSection .appendStr").each(function(){
             if( $(this).css("display")!=="none" ){
                 order=order+$(this).attr("data-showId")+',';
             }
+            console.log($(this))
         });
         order=order.substring(0,order.length-1);
 
         console.log(str,order);
 
-        $.ajax({
-            async : false,
-            cache:true,
-            type: 'POST',
-            dataType : "jsonp",
-            data:{strKey:str,order:order},  //参数
-            url:"http://192.168.31.156:8080/cmsNews/demo_data/test.do",//请求的action路径
-            error: function () {//请求失败处理函数
-
-            },
-            success:function(data){ //请求成功后处理函数。
-//                    alert("成功了")
-            }
-        });
+//        $.ajax({
+//            async : false,
+//            cache:true,
+//            type: 'POST',
+//            dataType : "jsonp",
+//            data:{strKey:str,order:order},  //参数
+//            url:"http://192.168.31.156:8080/cmsNews/demo_data/test.do",//请求的action路径
+//            error: function () {//请求失败处理函数
+//
+//            },
+//            success:function(data){ //请求成功后处理函数。
+////                    alert("成功了")
+//            }
+//        });
 
         var str=$('.main').html();
         str = str.replace(/<(script)[\S\s]*?\/\1>/gi, '');
         $('.main').html( str );
         $.zui.store.set('name', str);
-        window.location.href="container.html";
+        //window.location.href="container.html";
 
     })
 
