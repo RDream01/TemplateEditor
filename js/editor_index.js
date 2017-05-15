d857f405964b6bc193152921907d7039031b0e60
liuli0803 <2223204922@qq.com>
2017-05-15 19:08:02 +0800

    笑伟哥的组件

--- js/editor_index.js
+++ js/editor_index.js
@@ -383,22 +383,15 @@ function getTemplateCodeCallback(data){
 //    alert("离开")
 //}
 
-var MSG_UNLOAD="如果你此时离开档案系统，所做操作信息将全部丢失，是否离开?";
-var UnloadConfirm = {};
-//启用监听浏览器刷新、关闭的方法
-UnloadConfirm.set = function(confirm_msg){
-    window.onbeforeunload = function(event){
-        event = event || window.event;
-        event.returnValue = confirm_msg;
-    }
-}
-//关闭监听浏览器刷新、关闭的方法
-UnloadConfirm.clear = function(){
-    window.onbeforeunload = function(){};
-}
-UnloadConfirm.set(MSG_UNLOAD);
+//$(window).bind('beforeunload',function(){
+//    //return '您输入的内容尚未保存，确定离开此页面吗？';
+//});
 
-
+//window.onbeforeunload=onclose;
+//function onclose()
+//{
+//    return "您确定退出吗？";
+//}
 
 
 
