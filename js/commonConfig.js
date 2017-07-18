01503cf2b9d8b85d4fa743777470483d05f7b49a
liuli0803 <2223204922@qq.com>
2017-07-18 11:57:12 +0800

    头部增加--ll

--- js/commonConfig.js
+++ js/commonConfig.js
@@ -1,6 +1,6 @@
 /**
  * Created by ������ on 2017/5/26.
  */
-var basePath="http://124.207.72.8:8400/";
+var basePath="http://192.168.31.156:8400/";
 
 var blockBasePath = "http://124.207.72.8:8071";
